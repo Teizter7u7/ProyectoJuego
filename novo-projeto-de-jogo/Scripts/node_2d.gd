358c06badc01e06a7384cd8e5c4e2e6f638b6b67
NihilistM <teizter@gmail.com>
2026-09-08 00:50:17 -0300

    ñp

--- novo-projeto-de-jogo/Scripts/node_2d.gd
+++ novo-projeto-de-jogo/Scripts/node_2d.gd
@@ -5,8 +5,7 @@ extends Node2D
 func _ready() -> void:
 	print("ola")
 
-#prueba1
-#prueba2
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
